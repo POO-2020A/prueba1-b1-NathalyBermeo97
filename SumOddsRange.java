02256a4cfef157fc3542918158c1bf331fdf1e31
NathalyBermeo97 <66235614+NathalyBermeo97@users.noreply.github.com>
2020-07-06 11:38:28 -0500

    Update SumOddsRange.java

--- SumOddsRange.java
+++ SumOddsRange.java
@@ -1,38 +1,41 @@
+import java.util.ArrayList;
+
 import java.util.Scanner;
+public class PRUEBA2 {
 
-/**
-Pedir 2 numeros al usuario:
-
-int num1;
-int num2;
-
-
-Puedes asumir que num2 > num1
-
-Escribe un programa que calcule e imprima la suma de los numeros impares que estan entre num1 y num2 incluyendo estos numeros.
-
-Ejemplo de entrada/salida:
-#1: 3
-#2: 11
-35
-
-#1: 4
-#2: 20
-96
-
-#1: -2
-#2: 6
-8
-*/
-class SumOddsRange {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    System.out.print("#1: ");
-    int num1 = s.nextInt();
-    System.out.print("#2: ");
-    int num2 = s.nextInt();
-    //start below
+    public static void main(String[] args) {
     
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer>numbers=new ArrayList<>();
+        System.out.println("Ingrese un numero:");
+        int first= Integer.valueOf(scanner.nextLine());
+        System.out.println("Ingrese otro numero:");
+        int second= Integer.valueOf(scanner.nextLine());
+        printNumbers(numbers,first,second);
+        
+        
+    }
+    public static void printNumbers(ArrayList<Integer> numbers, int first, int second){
+        int i=0;
+        int suma=0;
+        
+        
+        for(i=first;i<=second;i++)
+        {
+           if(i%2 !=0)
+           {
+              
+               suma=suma+i;
+               
+           }
+            
+            
+        }
+        System.out.println("");    
+        System.out.println("La suma de los numeros impares es:"+ suma);
+        
+        
+        
+    }
     
-  }
 }
