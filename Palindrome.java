2babcf0eacb9dfbc170a5f896b97b99007440ba2
NathalyBermeo97 <66235614+NathalyBermeo97@users.noreply.github.com>
2020-07-06 11:57:04 -0500

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
+public class PRUEBA2 {
 
-/**
-Escribe un programa que pida una cadena al usuario y que revise si esta es un palindromo o no y terminar.
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Escribe una palabra:");
+        String cadena = scanner.nextLine();
+        
+        esPalindromo(cadena);
+        
+        System.out.println(esPalindromo(cadena));
+    }
+    
+    public static boolean esPalindromo(String cadena) {
+		
 
-Un palindromo significa que los caracteres de la cadena son los mismos al leer hacia adelante o hace atras.
-Debes ignorar los espacios, los acentos, puntuaciones y mayusculas y minusculas.
-
-Ejemplos de palindromos:
-Amor a Roma
-Bob
-Somos o no somos
-Acaso hubo buhos aca.
-Ana, la tacana catalana
-racecar
-was it a car or a cat I saw
-never odd or even
-rats live on no evil star
-
-El programa debe imprimir true si es un palindromo y false si no lo es.
-*/
-class Palindrome {
-  public static void main(String[] args) {
-    Scanner inp = new Scanner(System.in);
-    System.out.print("In:");
-    String s = inp.nextLine();
-   	//write your code below
-   	
-   	
-  }
+	cadena = cadena.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+		.replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+		
+	String invertida = new StringBuilder(cadena).reverse().toString();
+		
+	return invertida.equals(cadena);
+    }
 }
